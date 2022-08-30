482be9800c03a3a0dd4e417c4db3eb2ae8d1e3ef
go_green <adarshbolettin1997@gmail.com>
2022-08-30 23:44:27 +0530

    just checking git-hub actions

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,13 +1,9 @@
 """
 Calculator Functions
 """
-
-
-
 def add(x, y):
     """Add X and Y and then return results"""
     return x + y
-
 def subtract(x, y):
     """subtarct x from y and then return results"""
     return abs(x - y)

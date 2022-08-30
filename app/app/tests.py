77f46d3d028c4ddbc1e4858c25605252182c74e3
go_green <adarshbolettin1997@gmail.com>
2022-08-30 23:34:53 +0530

    just checking git-hub actions

---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+""" 
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the Calc Module"""
+
+    def test_add_numbers(self):
+        """Test adding two numbers"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+
+    def test_subtract_numbers(self):
+        """Subtracting Numbers"""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
